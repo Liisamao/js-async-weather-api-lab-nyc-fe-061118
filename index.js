755b4c0bea4b743df911104627dced0e8fbe86dd
Lisa Mao <liisamao@gmail.com>
2018-08-08 23:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "15c702583834f96c6f2ec5c6c4295eb9"
+const BASE_URL = 'https://api.openweathermap.org/data/2.3/weather?'
 
 function handleFormSubmit(event) {
+  event.preventDefault();
+  let city = document.getElementById("city").value
+  getWeatherFromApi(city)
   //handle submit event
 }
 
-function fetchCurrentWeather(city) {
+function getWeatherFromApi(city) {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + "&APPID=" + API_KEY + "&units=metric")
+  .then(response => response.json())
+  .then(displayWeather)
   //fetch current weather based on city
 }
 
-function displayCurrentWeather(json) {
+function displayWeather(weatherJson) {
+  let temp = weatherJson.main.temp
+  let tempRow = document.getElementById('temp')
+  tempRow.innerHTML = temp
   //render current weather data to the DOM using provided IDs and json from API
 }
 
@@ -26,5 +36,7 @@ function createChart(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  let cityForm = document.getElementById("cityForm");
+  cityForm.addEventListener("submit", handleFormSubmit)
   //add event listener here for form submission
 })
